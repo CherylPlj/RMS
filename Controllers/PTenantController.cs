6c9acf228b5fea6f99e010c2f154709c8f760af5
CherylPlj <cjeannepalaje@gmail.com>
2025-01-09 18:31:19 +0800

    Add project files.

---
+++ Controllers/PTenantController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMS.Controllers
+{
+    public class PTenantController : Controller
+    {
+        public IActionResult PTenantHomePage()
+        {
+            return View();
+        }
+
+        public IActionResult PTenantUnits()
+        {
+            return View();
+        }
+
+        public IActionResult PTenantDetails()
+        {
+            return View();
+        }
+        public IActionResult PTenantApply()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Controllers/StaffController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMS.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult SMaintenanceAssignment()
+        {
+            return View();
+        }
+        public IActionResult SMaintenanceHistory()
+        {
+            return View();
+        }
+    }
+}
