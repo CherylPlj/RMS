1435dcf0a3e4fca37863d5fdec4b5795e36640a4
CherylPlj <cjeannepalaje@gmail.com>
2025-01-11 22:57:59 +0800

    Gumagana yung login, yung register naman yung hindi hahaha

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -2,7 +2,7 @@
 
 namespace RMS.Models
 {
-    public class ApplicationDbContext : DbContext   
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
